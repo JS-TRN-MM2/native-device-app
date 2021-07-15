5d4fcba04cb16ee4e389676b95e131fef21730dc
Missy Maloney <missymaloney1@gmail.com>
2021-07-14 21:00:37 -0400

    camera takes pictures!

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,70 +1,72 @@
 import React, { useState } from 'react';
-import { 
-    ScrollView, 
-    View, 
-    Button, 
-    Text, 
-    TextInput, 
-    StyleSheet 
+import {
+  ScrollView,
+  View,
+  Button,
+  Text,
+  TextInput,
+  StyleSheet
 } from 'react-native';
 import { useDispatch } from 'react-redux';
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import Colors from '../constants/Colors';
 import * as placesActions from '../store/places-actions';
+import ImagePicker from '../components/ImagePicker';
 
 const NewPlaceScreen = props => {
-    const [titleValue, setTitleValue] = useState('');
+  const [titleValue, setTitleValue] = useState('');
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const titleChangeHandler = text => {
-        // you could add validation
-        setTitleValue(text);
-    };
+  const titleChangeHandler = text => {
+    // you could add validation
+    setTitleValue(text);
+  };
 
-    const savePlaceHandler = () => {
-        dispatch(placesActions.addPlace(titleValue));
-        props.navigation.goBack();
-    };
+  const savePlaceHandler = () => {
+    dispatch(placesActions.addPlace(titleValue));
+    props.navigation.goBack();
+  };
 
-    return (
-        <ScrollView>
-            <View style={styles.form}>
-                <Text style={styles.label}>Title</Text>
-                <TextInput 
-                    style={styles.textInput}
-                    onChangeText={titleChangeHandler}
-                    value={titleValue} />
-                <Button 
-                    title="Save Place" 
-                    color={Colors.primary} 
-                    onPress={savePlaceHandler}
-                />
-            </View>
-        </ScrollView>
-
-    );
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleChangeHandler}
+          value={titleValue}
+        />
+        <ImagePicker />
+        <Button
+          title="Save Place"
+          color={Colors.primary}
+          onPress={savePlaceHandler}
+        />
+      </View>
+    </ScrollView>
+  );
 };
 
 NewPlaceScreen.navigationOptions = {
-    headerTitle: 'Add Place'
+  headerTitle: 'Add Place'
 };
 
 const styles = StyleSheet.create({
-    form: {
-        margin: 30
-    },
-    label: {
-        fontSize: 18,
-        marginBottom: 15
-    },
-    textInput: {
-        borderBottomColor: '#ccc',
-        borderBottomWidth: 1,
-        marginBottom: 15,
-        paddingVertical: 4,
-        paddingHorizontal: 2
-    }
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
+  }
 });
 
 export default NewPlaceScreen;
